287f21b7659dd6719ebfe7ab2a46ed5d80ff4a15
baptiste geffroy <baptistegeffroy@macbook-pro-de-baptiste.home>
2022-07-29 16:30:23 +0200

    create route for all routes load index.html of client

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const path = require('path')
 require('dotenv').config()
 
 const port = process.env.PORT || 8000;
 
 const app = express();
 app.use(express.json())
+app.use(express.static('client/build'))
 
 app.get('/api', (req,res) => {
   res.send({
     msg: `L'api te dis bonjour 👋`
   })
+})
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'))
 })
 
 app.listen(port, (err) => {
